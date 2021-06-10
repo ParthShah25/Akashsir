5affd7b0259433dfce90a7a65425b8cd2ec97791
Parth <parthshah4363@gmail.com>
2021-06-10 16:48:00 +0530

    Career Guidance project Added with its CRUD & Postgresql importants screenshot.i will download the templates online and the details in each fields also written by the use of google. but operations performed by me

---
+++ myProject/account/urls.py
@@ -0,0 +1,8 @@
+from os import name
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('signin',views.signin,name='signin'),
+    path('signup',views.signup,name='signup'),
+]
---
+++ myProject/careerguidance/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Career
+
+# Register your models here.
+
+admin.site.register(Career)
